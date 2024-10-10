7a835f646950cacb58f3b5bbbde83a6a89b0ae84
Ercode Techoglu <ercode@pm.me>
2024-10-10 20:24:44 +0300

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell } from '@skeletonlabs/skeleton';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<AppShell>
+	<slot />
+</AppShell>
