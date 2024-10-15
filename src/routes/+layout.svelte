d1433555fd6fa1b61edf1a2e81b232111b4dadfc
Ercode Techoglu <ercode@pm.me>
2024-10-15 04:54:36 +0300

    add supabase ssr

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	export let data;
+	$: ({ session, supabase } = data);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
 	import '../app.postcss';
-	import { AppShell } from '@skeletonlabs/skeleton';
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,11 +18,6 @@
 		<div class="flex justify-center space-x-2">
 			<a class="btn variant-filled" href="/onboarding" rel="noreferrer">Hadi başlayalım</a>
 		</div>
-		<!-- <div class="space-y-2">
-			<p>Try editing the following:</p>
-			<p><code class="code">/src/routes/+layout.svelte</code></p>
-			<p><code class="code">/src/routes/+page.svelte</code></p>
-		</div> -->
 	</div>
 </div>
 
